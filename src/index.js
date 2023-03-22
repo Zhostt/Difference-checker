4f4d779a5f7b7687997b0e6fc46a87f294fda8bd
Zhostt <crazyplumber@yandex.ru>
2023-03-22 22:30:32 +0300

    Finished separating compareTreeFormer and Stylish finctions. Edited final GenDiff function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import fs from 'fs';
 import path, { dirname } from 'path';
 import stringParserToObject from './parsers.js';
+import { equal } from 'assert';
 
 // проверка расширений файлов, определение дальнешего пути работы
 export const checkFileExtension = (pathGiven) => {
@@ -27,60 +28,79 @@ export const pathAbsolutizer = (pathGiven) => {
 // Извлекаем строку из файла по указанному пути
 export const fileStringExtractor = (absPath) => fs.readFileSync(absPath, 'UTF-8');
 
-// Сравнение 2 плоских объектов, вывод строки
-export const compareObjects = (file1, file2) => {
-  const keysAll = Object.keys(file1).concat(Object.keys(file2));
-  const keysAllUniq = [...new Set(keysAll)].sort();
-  if (keysAll.length === 0) {
-    return '{}';
-  }
-  const resultString = keysAllUniq.reduce((acc, key) => {
-    if (file1[key] === file2[key]) {
-      acc += `   ${key}: ${file1[key]}\n`;
-    } else if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-      acc += ` - ${key}: ${file1[key]}\n`;
-    } else if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-      acc += ` + ${key}: ${file2[key]}\n`;
-    } else if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)
-    && file1[key] !== file2[key]) {
-      acc += ` - ${key}: ${file1[key]}\n`;
-      acc += ` + ${key}: ${file2[key]}\n`;
-    }
-    return acc;
-  }, '');
-  return `{\n${resultString}}`;
-};
+// Формирователь АСТ дерева сравнений из 2 объектов
+export const compareTreeFormer = (file1, file2) => {
+  const [removed, added, equal, modified, nested, stringified1, stringified2] = ['removed', 'added', 'equal', 'modified', 'nested', 'stringified1', 'stringified2']
+    // node structure = {key, status, depth, value1, value2}
+    // statuses: removed, added, equal, modified, stringified1, stringified2
+    const innerTreeFormer = (file1, file2, depth) => {
+      const keysAll = Object.keys(file1).concat(Object.keys(file2));
+      const keysAllUniq = [...new Set(keysAll)].sort();
 
-// сравнение объектов (в тч вложенных)
-export const stylish = (file1, file2, depth = 1, space = '    ') => {
-  const [removed, added, equal] = ['- ', '+ ', '  '];
-  const margin = space.repeat(depth).slice(0, -equal.length);
-  const keysAll = Object.keys(file1).concat(Object.keys(file2));
-  const keysAllUniq = [...new Set(keysAll)].sort();
+      // Итерирующая каждый ключ функция
+      const iter = (key, depth) => {
+        const value1 = file1[key];
+        const value2 = file2[key];
+        // Если оба значения - не объекты
+        if (!_.isObject(value1) && !_.isObject(value2)) {
+          if (value1 === value2) {
+            return({key: key, status: equal, depth: depth, value1: value1, value2: value2})
+          } else if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+            return({key: key, status: removed, depth: depth, value1: value1})
+          } else if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+            return({key: key, status: added, depth: depth, value2: value2})
+          } else if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)
+          && value1 !== value2) {
+            return({key: key, status: modified, depth: depth, value1: value1, value2: value2})
+          }
+        // Тут нужна рекурсия - если оба значения = объекты
+        } else if (_.isObject(value1) && _.isObject(value2)) {
+          return({key: key,status: nested, depth: depth, value1: innerTreeFormer(value1, value2, depth + 1) })
 
-  if (keysAll.length === 0) {
-    return '{}';
-  }
-
-  const stringify = (obj, innerDepth) => {
-    const innerMargin = space.repeat(innerDepth).slice(0, -equal.length);
-    const marginNestedStr = innerMargin + space + innerMargin.slice(0, equal.length);
-    const objString = Object.keys(obj)
-      .reduce((acc, key) => {
-        if (!_.isObject(obj[key])) {
-          acc += `\n${marginNestedStr}${key}: ${obj[key]}`;
-          return acc;
+        // Если первое значение объект, второе нет
+        } else if (((_.isObject(value1) && !_.isObject(value2)))) {
+          return({key: key, status: stringified1, depth: depth, value1: value1, value2: value2 })
+          }
+        // Если второе значение объект, первое нет
+          else if ((!_.isObject(value1) && _.isObject(value2))) {
+          return({key: key, status: stringified2, depth: depth, value1: value1, value2: value2 })
         }
-        acc += `\n${marginNestedStr}${key}: ${stringify(obj[key], innerDepth + 1)}`;
+      };
+      return keysAllUniq.map((key) => iter(key, depth));
+  }
+  return innerTreeFormer(file1, file2, 1)
+}
+
+const stylish = (array, space = '    ') => {
+  // node structure = {key, status, depth, value1, value2}
+  // statuses: removed, added, equal, modified, stringified1, stringified2
+  const spaceLength = space.length;
+  const [removedSign, addedSign, equalSign] = ['- ', '+ ', '  '];
+  const [removed, added, equal, modified, nested, stringified1, stringified2] = ['removed', 'added', 'equal', 'modified', 'nested', 'stringified1', 'stringified2']
+  const leftMargin = equalSign.length;
+
+  // Обработка случая, когда значения ключа с одной стороны - Объект, с другой - нет. Выдача объекта строкой.
+  const stringifyObj = (obj, innderDepth) => {
+    const margin = space.repeat(innderDepth)
+    const objString = Object.keys(obj)
+    .reduce((acc, key) => {
+      const value = obj[key];
+      if (!_.isObject(value)) {
+        acc += `\n${margin}${key}: ${value}`;
         return acc;
-      }, '');
-    const resultString = `{${objString}\n${innerMargin + innerMargin.slice(0, equal.length)}}`;
+      }
+      acc += `\n${margin}${key}: ${stringifyObj(value, innderDepth + 1)}`;
+      return acc;
+    }, '')
+    const resultString = `{${objString}\n${margin.slice(0, -spaceLength)}}`;
     return resultString;
   };
 
-  const resultString = keysAllUniq.reduce((acc, key) => {
-    const value1 = file1[key];
-    const value2 = file2[key];
+  // Обработка объекта - ребенка из массива value
+  const iter = (acc, object) => {
+    const {key, status, depth, value1, value2} = object;
+    const margin = space.repeat(depth).slice(0, -leftMargin)
+    const marginSizeOfEqualSign = margin.slice(0, leftMargin)
     let [space1, space2] = [' ', ' '];
     if (value1 === '') {
       space1 = '';
@@ -88,38 +108,45 @@ export const stylish = (file1, file2, depth = 1, space = '    ') => {
     if (value2 === '') {
       space2 = '';
     }
-    if (!_.isObject(value1) && !_.isObject(value2)) {
-      if (file1[key] === file2[key]) {
-        acc += `${margin}${equal}${key}:${space1}${file1[key]}\n`;
-      } else if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-        acc += `${margin}${removed}${key}:${space1}${file1[key]}\n`;
-      } else if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-        acc += `${margin}${added}${key}:${space2}${file2[key]}\n`;
-      } else if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)
-      && file1[key] !== file2[key]) {
-        acc += `${margin}${removed}${key}:${space1}${file1[key]}\n`;
-        acc += `${margin}${added}${key}:${space2}${file2[key]}\n`;
-      }
-    } else if (_.isObject(value1) && _.isObject(value2)) {
-      acc += `${margin}  ${key}: ${stylish(value1, value2, depth + 1)}\n`;
-    } else if (((_.isObject(value1) && !_.isObject(value2)))) {
-      acc += `${margin}${removed}${key}:${space1}${stringify(value1, depth)}\n`;
-      if (value2 !== undefined) {
-        acc += `${margin}${added}${key}:${space2}${(value2)}\n`;
-      }
-    } else if ((!_.isObject(value1) && _.isObject(value2))) {
-      if (value1 !== undefined) {
-        acc += `${margin}${removed}${key}:${space1}${(value1)}\n`;
-      }
-      acc += `${margin}${added}${key}:${space2}${(stringify(value2, depth))}\n`;
+
+    switch (status){
+      case removed:
+        return acc += `${margin}${removedSign}${key}:${space1}${value1}\n`;
+      case added:
+        return acc += `${margin}${addedSign}${key}:${space2}${value2}\n`;
+      case equal:
+        return acc += `${margin}${equalSign}${key}:${space1}${value1}\n`
+      case modified:
+        acc += `${margin}${removedSign}${key}:${space1}${value1}\n`;
+        acc += `${margin}${addedSign}${key}:${space2}${value2}\n`;
+        return acc;
+      case nested:
+        return acc += `${margin}${equalSign}${key}:${space1}{\n${stylish(value1)}${margin + marginSizeOfEqualSign}}\n`
+      case stringified1:
+        acc += `${margin}${removedSign}${key}:${space1}${stringifyObj(value1, depth+1)}\n`;
+        if (value2 !== undefined) {
+          acc += `${margin}${addedSign}${key}:${space2}${(value2)}\n`;
+        }
+        return acc;
+      case stringified2:
+        if (value1 !== undefined) {
+          acc += `${margin}${removedSign}${key}:${space1}${(value1)}\n`;
+        }
+        acc += `${margin}${addedSign}${key}:${space2}${stringifyObj(value2, depth+1)}\n`;
+        return acc;
     }
-
     return acc;
-  }, '');
-  return `{\n${resultString}${margin.slice(0, -equal.length)}}`;
-};
+  }
 
-export const genDiff = (path1, path2) => {
+  let result = '';
+  for (const object of array){
+    result += iter('', object)
+  }
+
+  return result;
+}
+
+export const genDiff = (path1, path2, format = 'stylish') => {
   if (path1.length === 0 || path2.length === 0) {
     return 'enter valid path';
   }
@@ -128,7 +155,11 @@ export const genDiff = (path1, path2) => {
 
   const object1 = stringParserToObject(dataString1, checkFileExtension(pathAbs1));
   const object2 = stringParserToObject(dataString2, checkFileExtension(pathAbs2));
-  return stylish(object1, object2);
+  if (format === 'stylish')
+    return `{\n${stylish(compareTreeFormer(object1, object2))}}`;
 };
 
-
+/* for quick  test purposes
+const obj1 = {a: {b: {c: 1}}, d: 'e', modded: '1', f: {nestedOne: 'object'}, g: 'notNestedFirst', h: {nested: 'firstOnly'}, removed: 'first'}
+const obj2 = {a: {b: {c: 2}}, d: 'e', modded: '2', f: 'notObj', g: {nested: 'second'}, added: 'second'}
+*/
